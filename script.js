4aefff2bafd1a9e42d935d9e3aaf407750698c2f
Dipesh Trikam <dipesht@datacom.co.nz>
2017-09-08 08:51:52 +1200

    printing result to console

--- script.js
+++ script.js
@@ -21,22 +21,31 @@ function myFunction() {
 }
 
 function result(arr) {
-  var count;
-  for (count = 0; count < arr.length; count++) {
-    // Change attribute names here
-    personHardHatTimestamps.push(arr[count].userId)
-    personHighVisibilityVestTimestamps.push(arr[count].title)
-    personHardHatHighVisibilityVestTimestamps.push(arr[count].body)
-  }
+  // var count;
+  // for (count = 0; count < arr.length; count++) {
+  //   // Change attribute names here
+  //   personHardHatTimestamps.push(arr[count].Timestamp)
+  //   personHighVisibilityVestTimestamps.push(arr[count].title)
+  //   personHardHatHighVisibilityVestTimestamps.push(arr[count].person_hard_hat_high_vis_vest)
+  // }
+  // createLink(arr[arr.length - 1].video)
+
+  console.log(arr);
 
   myFunction();
+}
+
+function createLink(videoURL){
+  var str = "Click here to download output video";
+  var result = str.link(videoURL);
+  document.getElementById("videoDownloadLink").innerHTML = result;
 }
 
 function myRequest(){
 
   var xmlhttp = new XMLHttpRequest();
   // Endpoint URL
-  var url = "https://jsonplaceholder.typicode.com/posts";
+  var url = "https://yolostorage.blob.core.windows.net/report/report.json";
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -47,4 +56,19 @@ function myRequest(){
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 
+
+  // var uri = "https://yolostorage.blob.core.windows.net/report/report.json"
+  //       $.ajax({
+  //           url: uri,
+  //           beforeSend: function (request) {
+  //               request.setRequestHeader("Authorization", "Negotiate");
+  //           },
+  //           async: true,
+  //           success: function (data) {
+  //               alert(JSON.stringify(data));
+  //           },
+  //           error: function (xhr, textStatus, errorMessage) {
+  //               alert(errorMessage);
+  //           }
+  //       });
 }
